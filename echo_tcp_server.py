f30b741276b30bf362620efe13534c10047e11c9
sharvadim07 <sharvadim07@yandex.ru>
2023-04-25 15:59:43 +0700

    Several processes

--- echo_tcp_server.py
+++ echo_tcp_server.py
@@ -1,18 +1,28 @@
 import socket
+import os
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind(("0.0.0.0", 2222))
-    s.listen(10)
-    while True:
-        conn, addr = s.accept()
-        print(f"new connection with {addr}")
-        data = conn.recv(1024)
-        print(f"recieved data = {data.decode()}")
-        if not data or data.decode() == "close":
-	    print(f"close connection with {addr}")
-	    conn.close()
-            break
-	print(f"send data to {addr}")
-        conn.sendall(data)
-        print(f"close connection with {addr}")
-        conn.close()
+    max_connections = 10
+    s.listen(max_connections)
+    for i in range(max_connections):
+        worker_pid = os.fork()
+        if not worker_pid:
+            conn, addr = s.accept()
+            print(f"new connection with {addr}")
+            data = conn.recv(1024)
+            print(f"recieved data = {data.decode()}")
+            if not data or data.decode() == "close":
+                print(f"close connection with {addr}")
+                conn.close()
+                exit(0)
+            print(f"send data to {addr}")
+            conn.sendall(data)
+            print(f"close connection with {addr}")
+            conn.close()
+            exit(0)
+        else:
+            print(f"process started {worker_pid}")
+    for i in range(max_connections):
+       exit_pit, exit_status = os.wait()
+       print(f"process finished {exit_pit} with status {exit_status}")
